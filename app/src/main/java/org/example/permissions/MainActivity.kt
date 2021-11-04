0c83807b0d908e5f1fc9703711c215d439ee9345
Wim Hambrouck <wim.hambrouck@ehb.be>
2021-11-04 14:55:31 +0100

    initial commit

---
+++ app/src/main/java/org/example/permissions/MainActivity.kt
@@ -0,0 +1,17 @@
+package org.example.permissions
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
